dc8b06ec49b80da494d47d27b1cbc367fc29db7c
whileNazaTrue <nazarenoorodriguez@gmail.com>
2022-08-14 18:34:40 -0300

    Working on: Posts and profiles

---
+++ src/database/Profile.js
@@ -0,0 +1,74 @@
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+
+const getAllProfiles = async () => {
+    const profiles = await prisma.profile.findMany({
+        include: {
+            user:{
+                select: { id: true, username: true, password: false }
+            },
+        },
+    })
+    return profiles
+}
+
+const getOneProfileById = async (id) => {
+    const profile = await prisma.profile.findUnique({
+        where: {
+            id: Number.parseInt(id)
+        },
+        include: {
+            user: true
+        }
+    })
+    return profile
+}
+
+const getOneProfileByUsername = async (username) => {
+    const profile = await prisma.profile.findUnique({
+        where: {
+            username: username
+        },
+        include: {
+            user: true
+        }
+    })
+    return profile
+}
+
+const createProfile = async (profile) => {
+    console.log(profile)
+    const newProfile = await prisma.profile.create({
+        data: profile
+    })
+    return newProfile
+}
+
+const updateProfile = async (id, profile) => {
+    const updatedProfile = await prisma.profile.update({
+        where: {
+            id: Number.parseInt(id)
+        },
+        data: profile
+    })
+    return updatedProfile
+}
+
+const deleteProfile = async (id) => {
+    const deletedProfile = await prisma.profile.delete({
+        where: {
+            id: Number.parseInt(id)
+        }
+    })
+    return deletedProfile
+}
+
+export default {
+    getAllProfiles,
+    getOneProfileById,
+    getOneProfileByUsername,
+    createProfile,
+    updateProfile,
+    deleteProfile
+}
